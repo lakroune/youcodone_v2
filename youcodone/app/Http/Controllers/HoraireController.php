827c84c6840b6d91b94c77ff1b1e5b05f9ab9b00
lakroune <lakroune00@gmail.com>
2026-02-02 15:41:27 +0100

    Refactor test suite to improve structure and readability; updated test cases to align with Pest syntax.

---
+++ youcodone/app/Http/Controllers/HoraireController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Horaire;
+use App\Http\Requests\StoreHoraireRequest;
+use App\Http\Requests\UpdateHoraireRequest;
+
+class HoraireController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreHoraireRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Horaire $horaire)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Horaire $horaire)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateHoraireRequest $request, Horaire $horaire)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Horaire $horaire)
+    {
+        //
+    }
+}
---
+++ youcodone/app/Http/Controllers/TypeCuisineController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TypeCuisine;
+use App\Http\Requests\StoreTypeCuisineRequest;
+use App\Http\Requests\UpdateTypeCuisineRequest;
+
+class TypeCuisineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTypeCuisineRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TypeCuisine $typeCuisine)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TypeCuisine $typeCuisine)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTypeCuisineRequest $request, TypeCuisine $typeCuisine)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TypeCuisine $typeCuisine)
+    {
+        //
+    }
+}
