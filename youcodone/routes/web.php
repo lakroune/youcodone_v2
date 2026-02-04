0b5c8d36f3640aac11305df01231f8f4dd5b0a83
lakroune <lakroune00@gmail.com>
2026-02-04 09:41:51 +0100

    Update web routes to include DashboardController for dashboard view, replace dashboard closure with controller method, and streamline restaurant routes using resource controller.

--- youcodone/routes/web.php
+++ youcodone/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardConteroller;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RestaurantController;
 use App\Models\Restaurant;
@@ -9,10 +10,7 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
+ 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -26,9 +24,8 @@ Route::middleware('client')->group(function () {
 });
 
 Route::middleware('restaurateur')->group(function () {
-    Route::get('/restaurateur-area', function () {
-        return view('restaurateur.area');
-    })->name('restaurateur.area');
+    Route::resource('restaurants', RestaurantController::class);
+    Route::get('/dashboard', [DashboardConteroller::class, 'index'])->name('dashboard');
 });
 
 require __DIR__ . '/auth.php';
